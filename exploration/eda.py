c22356d9aacaf0e163e018986a9a260e5397c563
Kylexi <mmcampbell0@gmail.com>
2020-10-18 11:23:36 -0400

    Cleaning eda with markdowns and filters.

--- exploration/eda.py
+++ exploration/eda.py
@@ -3,19 +3,36 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 
+min_price = st.sidebar.slider('Min Price', 0, 50, 0)
+max_price = st.sidebar.slider('Max Price', 50, 200, 200)
+
+# Read in data from PSQL
 df, df_imgs, df_bids = get_dfs()
-df = df[df.price < 100]
+
+# Filtering down by price
+df = df[(df.price > min_price) & (df.price <= max_price)]
+
 st.markdown("## Master dataframe")
 df
-df['cond'].value_counts().plot(kind='bar')
-st.pyplot()
-df.plot(kind='scatter', x='cond', y='price')
-st.pyplot()
+
+st.markdown('## Price Distribution')
 df['price'].hist(bins=20)
 st.pyplot()
+
+st.markdown('## Condition Counts')
+df['cond'].value_counts().plot(kind='bar')
+st.pyplot()
+
+st.markdown('## Price vs Condition')
+df.plot(kind='scatter', x='cond', y='price')
+st.pyplot()
+
+st.markdown('## Price vs Bid Duration')
 df.plot(kind='scatter', x='bid_duration', y='price')
 st.pyplot()
-df['rating_count'] = df['rating_count'].astype(str)
+
+st.markdown('## Price vs Rating Count')
+# df['rating_count'] = df['rating_count'].astype(str)
 df.plot(kind='scatter', x='rating_count', y='price')
 st.pyplot()
 
